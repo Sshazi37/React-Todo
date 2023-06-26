d90b14681fd70d55da2fa14e8dc41c765738dcdb
Muhammad Shahzaib <108273375+Sshazi37@users.noreply.github.com>
2023-06-26 17:07:01 +0500

    validation code

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,25 +7,37 @@ function Home() {
 
     const [data , setData] = useState(''); // value of input field
     const [tasks , setTask] = useState([]); // value of task on click
+    const [validation, setValidation] = useState();
 
 
     function handleChange(e){     
         setData(e.target.value);     
         }
     function addTodo() {
-        setTask([...tasks, data]);   
-        setData(''); 
+        if(data === ""){
+            setValidation("Enter Something");
+        }else 
+        {
+            setTask([...tasks, data]);   
+            setData(''); 
+            setValidation("");
+        }
+
+
         }
     const deleteTodo = (index) =>{       
         const updatedTasks = [...tasks];
         updatedTasks.splice(index, 1);
         setTask(updatedTasks);
     }
+
+
   return (
     <>
         <div className='container'>
             <section className='mt-3 text-center' >
                 <h3>Enter Your Task</h3>
+                <p>{validation}</p>
                 <div className='todo col-lg-5 mx-auto d-flex justify-content-center align-items-center'>
                     <input className='form-control' name= "task" value={data} onChange={handleChange} /> 
                     <Button variant='contained' className='mx-2' onClick={addTodo}><AddIcon /></Button>
@@ -37,7 +49,7 @@ function Home() {
                 
             </section>
         </div>
-        
+
     </>
   )
 }
